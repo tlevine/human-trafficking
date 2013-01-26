5ea9ad6723af15653b553e70a3be44d2f864e5e5
Thomas Levine <occurrence@thomaslevine.com>
2013-01-26 15:21:15 -0500

    aoeu

---
+++ download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_1/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_2/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_3/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_4/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_5/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_6/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_7/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_8/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_9/
+wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_10/
+wget -O minneapolis.db  https://scraperwiki.com/scrapers/export_sqlite/its_a_start_11/
