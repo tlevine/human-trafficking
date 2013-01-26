1557d83fa86584142c046b12a40e77b07b5b4897
Thomas Levine <occurrence@thomaslevine.com>
2013-01-26 15:26:27 -0500

    aoeu

--- download.sh
+++ download.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_1/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_2/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_3/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_4/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_5/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_6/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_7/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_8/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_9/
-wget -O   https://scraperwiki.com/scrapers/export_sqlite/its_a_start_10/
+wget -O dc.db  https://scraperwiki.com/scrapers/export_sqlite/its_a_start_1/
+wget -O philidelphia.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_2/
+wget -O losangeles.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_3/
+wget -O denver.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_4/
+wget -O seattle.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_5/
+wget -O portland.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_6/
+wget -O newyork.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_7/
+wget -O miami.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_8/
+wget -O newjersey.db https://scraperwiki.com/scrapers/export_sqlite/its_a_start_9/
+wget -O chicago.db  https://scraperwiki.com/scrapers/export_sqlite/its_a_start_10/
 wget -O minneapolis.db  https://scraperwiki.com/scrapers/export_sqlite/its_a_start_11/
